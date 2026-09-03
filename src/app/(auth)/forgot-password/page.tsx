a52b4762bb0256d5c8b1a699aff2247ffc7cb0b4
0xkid-root <prohackers698@gmail.com>
2026-09-03 13:30:55 +0530

    file structure setup

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1>Forgot Password Placeholder</h1>
+    </div>
+  );
+}
---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ResetPasswordPage() {
+  return (
+    <div>
+      <h1>Reset Password Placeholder</h1>
+    </div>
+  );
+}
