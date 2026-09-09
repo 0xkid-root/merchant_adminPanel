4afbf16304716498cb167c239a5df4a6400918ad
0xkid-root <prohackers698@gmail.com>
2026-09-09 11:50:21 +0530

    font issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className={`${inter.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="font-sans min-h-full flex flex-col">
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
